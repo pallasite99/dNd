4a01d217aad942cc477195ac0d32674a6ecab23a
Salil Apte <salil.apte99@gmail.com>
2023-06-15 08:17:16 +0530

    refactored mysql code to use pool queries

--- src/app.js
+++ src/app.js
@@ -1,79 +1,66 @@
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const routes = require('./routes/index');
 
-// Create MySQL connection
-const connection = mysql.createConnection({
+// Create MySQL connection pool
+const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'dnd',
+    connectionLimit: 10, // Adjust the limit as per requirements
 });
 
-// Connect to MySQL
-connection.connect((err) => {
-    if (err) {
-      console.error('Error connecting to MySQL:', err);
-      return;
+// Utility function to execute a MySQL query
+const executeQuery = async (query) => {
+    try {
+      const connection = await pool.getConnection();
+      const [results] = await connection.query(query);
+      connection.release();
+      return results;
+    } catch (error) {
+      throw new Error('Error executing MySQL query');
     }
-    console.log('Connected to MySQL');
-});
+  };
 
 // Create express app
 const app = express();
 
+// Middleware to handle errors
+app.use((err, req, res, next) => {
+    console.error('Error:', err);
+    res.status(500).json({ error: 'Internal server error' });
+});
+
 // Races endpoint
-app.get('/races', async (req, res, next) => {
+app.get('/races', async (req, res) => {
     try {
         const query = 'SELECT * FROM races';
-
-        connection.query(query, (err, results) => {
-            if (err) {
-                console.error('Error executing MySQL query:', err);
-                res.status(500).json({ error: 'Internal server error' });
-                return;
-            }
-
-            res.json(results);
-        });
+        const results = await executeQuery(query);
+        res.json(results);
     } catch (error) {
+        console.log(error);
         res.status(400).send({error: 'Bad Request'});
     }
 });
 
 // List gender endpoint
-app.get('/gender', async (req, res, next) => {
+app.get('/gender', async (req, res) => {
     try {
         const query = 'SELECT name, description FROM gender';
-
-        connection.query(query, (err, results) => {
-            if (err) {
-                console.error('Error executing MySQL query:', err);
-                res.status(500).json({ error: 'Internal server error' });
-                return;
-            }
-
-            res.json(results);
-        });        
+        const results = await executeQuery(query);
+        res.json(results);        
     } catch (error) {
         res.status(400).send({error: 'Bad Request'});
     }
 });
 
 // List classes endpoint
-app.get('/classes', async (req, res, next) => {
+app.get('/classes', async (req, res) => {
     try {
         const query = 'SELECT name, hit_die, spellcasting FROM classes';
-
-        connection.query(query, (err, results) => {
-            if (err) {
-                console.error('Error executing MySQL query:', err);
-                res.status(500).json({ error: 'Internal server error' });
-                return;
-            }
-
-            res.json(results);
-        });
+        const results = await executeQuery(query);
+        res.json(results);
     } catch (error) {
         res.status(400).send({error: 'Bad Request'});
     }
